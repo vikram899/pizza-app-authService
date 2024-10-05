c035f5efc941c2912ec83beacd9097d7adf875fe
vikram899 <vikramdeshmukh899@gmail.com>
2024-10-05 19:25:58 +0530

    Add ES lint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["**/dist/*"] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-console": "error",
+    },
+  }
+);
